d1b3485604849193b0e9481d162b7379f416e85d
Khabane Lekena <khabane.lekena@gmail.com>
2025-12-18 13:49:54 -0500

    update to new vs and include libgltf

--- TAS/compile.bat
+++ TAS/compile.bat
@@ -1,4 +1,4 @@
 md .\shaders_compiled
-D:/VulkanSDK/1.1.121.0/Bin32/glslc.exe shaders/shader.vert -o TAS/shaders_compiled/vert.spv
-D:/VulkanSDK/1.1.121.0/Bin32/glslc.exe shaders/shader.frag -o TAS/shaders_compiled/frag.spv
+C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shaders/shader.vert -o shaders_compiled/vert.spv
+C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shaders/shader.frag -o shaders_compiled/frag.spv
 pause
