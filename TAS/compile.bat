ec9f268fa99098f3e20877932fb73a7f01aa9821
Khabane Lekena <khabane.lekena@gmail.com>
2026-06-24 15:57:16 -0400

    update vulkan version

--- TAS/compile.bat
+++ TAS/compile.bat
@@ -1,4 +1,4 @@
 md .\shaders_compiled
-C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shaders/shader.vert -o shaders_compiled/vert.spv
-C:\VulkanSDK\1.4.313.2\Bin\glslc.exe shaders/shader.frag -o shaders_compiled/frag.spv
+"%VULKAN_SDK%\Bin\glslc.exe" shaders/shader.vert -o shaders_compiled/vert.spv
+"%VULKAN_SDK%\Bin\glslc.exe" shaders/shader.frag -o shaders_compiled/frag.spv
 pause
